acdc13a295565dc816fe3115b7b6fad0f00a7937
Joseph Parker <jparker010190@gmail.com>
2018-09-01 19:50:17 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,6 +9,6 @@ class RepositoriesController < ApplicationController
       @stocks = Faraday.get 'https://api.iextrading.com/1.0/tops/last?symbols=SNAP,fb,AIG%2b'
       @body = JSON.parse(@stocks.body)
     end
-  render 'search'
-end
+    render 'search'
+  end
 end
