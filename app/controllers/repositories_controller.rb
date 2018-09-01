46697dd6f8e3d2af50c4205984cbd35876615b10
Joseph Parker <jparker010190@gmail.com>
2018-09-01 19:50:00 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,22 +6,9 @@ class RepositoriesController < ApplicationController
 
   def github_search
     begin
-    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-      req.params['client_id'] = 'v1.c5f032e339de8903'
-      req.params['client_secret'] = '914cf3f0c0c5f416a17ec435261283d74f4d8332'
-      req.params['q'] = params[:query]
+      @stocks = Faraday.get 'https://api.iextrading.com/1.0/tops/last?symbols=SNAP,fb,AIG%2b'
+      @body = JSON.parse(@stocks.body)
     end
-    @stocks = Faraday.get 'https://api.iextrading.com/1.0/tops/last?symbols=SNAP,fb,AIG%2b'
-    body = JSON.parse(@resp.body)
-    @body = JSON.parse(@stocks.body)
-  if @resp.success?
-    @results = body["items"]
-  else
-    @error = body["meta"]["errorDetail"]
-  end
-  rescue Faraday::TimeoutError
-      @error = "There was a timeout. Please try again."
-  end
   render 'search'
 end
 end
